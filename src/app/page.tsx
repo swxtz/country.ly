87898b2d42cc9200b03e98cbd8f162e48daf9ecd
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-15 20:13:16 -0300

    feat: fetch countries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+
+interface country {
+    name: {
+        common: string;
+    }
+}
+
+async function getContries(): Promise<country[]> {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    return response.json();
+}
+
+export default async function Home() {
+    const countries = await getContries();
+
     return (
         <section className="container flex  w-full">
-            <h1>asdasd</h1>
+            {countries.map((country) => ( <h1 key={country.name.common}>{country.name.common}</h1> ))}
         </section>
     );
 }
